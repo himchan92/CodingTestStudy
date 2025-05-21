a64abb7447139bad5f8ebf41720b75452f799195
himchan92 <omea92@naver.com>
2025-05-21 12:51:14 +0900

    프로그래머스 - lv1 - 자연수 뒤집어 배열로 만들기

--- src/main/java/programmers/lv1/Solution1.java
+++ src/main/java/programmers/lv1/Solution1.java
@@ -1,8 +1,25 @@
 package programmers.lv1;
 
+/**
+ * 자연수 뒤집어 배열로 만들기
+ * https://school.programmers.co.kr/learn/courses/30/lessons/12932
+ */
 public class Solution1 {
 
-    public static void main(String[] args) {
+    public static int[] solution(long n) {
+        int index = 0;
 
+        String[] str = String.valueOf(n).split("");
+        int[] answer = new int[str.length];
+
+        for(int i = str.length-1; i >= 0; i--) {
+            answer[index++] = Integer.parseInt(str[i]);
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(12345));
     }
 }
