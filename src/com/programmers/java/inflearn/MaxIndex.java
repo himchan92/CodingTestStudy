845aa51362c5fac77ceb7b08ddce29ed50dc26f5
omea92 <thdjun92@naver.com>
2025-12-05 23:17:41 +0900

    lv1풀이

---
+++ src/com/programmers/java/inflearn/MaxIndex.java
@@ -0,0 +1,13 @@
+package com.programmers.java.inflearn;
+
+public class MaxIndex {
+    public static void main(String[] args) {
+        int[] arr = new int[]{1, 3, 5, 4, 5, 2, 1};
+        System.out.println(solution(arr));
+    }
+
+    public static int[] solution(int[] arr) {
+        int[] answer = {};
+        return answer;
+    }
+}
